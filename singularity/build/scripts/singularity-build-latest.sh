2d5d21ce096f49d62de5fe440505f39030265b62
vsoch <vsochat@stanford.edu>
2017-01-02 03:49:11 -0800

    removing autogen.sh

--- singularity/build/scripts/singularity-build-latest.sh
+++ singularity/build/scripts/singularity-build-latest.sh
@@ -4,13 +4,14 @@ sudo apt-get -y install git \
                         build-essential \
                         libtool \
                         autotools-dev \
+                        linux-headers-generic \
                         automake \
                         autoconf \
                         python3-pip
 
 # Install Singularity from Github
 cd /tmp && git clone http://www.github.com/singularityware/singularity &&
-   cd singularity && ./autogen.sh && ./configure --prefix=/usr/local && make && sudo make install
+   cd singularity && ./configure --prefix=/usr/local && make && sudo make install
 
 # Pip3 installs
 sudo pip3 install --upgrade pip &&
