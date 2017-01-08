8ad0a9df371bbf63ec86d8af4986d5e46c1a13c9
vsoch <vsochat@stanford.edu>
2017-01-08 15:56:21 -0800

    adding docker os comparison image

--- examples/package_tree/plot_similarity.py
+++ examples/package_tree/plot_similarity.py
@@ -1,6 +1,9 @@
 # A quick example of making a package tree with data derived from calculate_similarity.py
+# Set environment variable MESSAGELEVEL to CRITICAL to disable debug output
 
 from singularity.views.trees import make_package_tree
+from singularity.analysis.compare import compare_packages
+from singularity.analysis.utils import get_packages
 
 # Compare your own data
 data = pickle.load(open('comparisons.pkl','rb'))['files.txt']
@@ -10,7 +13,7 @@ plt = make_package_tree(matrix=data)
 package_set1 = get_packages(family='docker-os')
 package_set2 = get_packages(family='docker-os')
 data = compare_packages(packages_set1=package_set1,
-                        packages_set2=packge_set2)['files.txt']
+                        packages_set2=package_set2)['files.txt']
 
 plt = make_package_tree(matrix=data)
 
