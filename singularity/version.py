ea6361d14645068494fd26f9750a1403bc8483a0
vsoch <vsochat@stanford.edu>
2017-06-01 23:39:36 -0400

    updating main build to not use in memory tar, not suitable for singularity hub

--- singularity/version.py
+++ singularity/version.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-__version__ = "1.1.9"
+__version__ = "1.2.0"
 AUTHOR = 'Vanessa Sochat'
 AUTHOR_EMAIL = 'vsochat@stanford.edu'
 NAME = 'singularity'
