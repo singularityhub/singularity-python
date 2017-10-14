5a9c3d9891304fb77122007a50e6642e1c6bd1eb
vsoch <vsochat@stanford.edu>
2017-10-14 16:57:29 -0700

    adding different build options / templates

--- singularity/build/scripts/singularity-build-dev.sh
+++ singularity/build/scripts/singularity-build-dev.sh
@@ -8,6 +8,14 @@
 #
 ################################################################################
 
+# Install Singularity from Github
+cd /tmp && git clone -b feature-squashbuild-secbuild https://github.com/cclerget/singularity.git &&
+cd /tmp/singularity && ./autogen.sh && ./configure --prefix=/usr/local && make && sudo make install && sudo make secbuildimg
+
+# Singularity python development
+cd /tmp && git clone -b development https://www.github.com/vsoch/singularity-python.git &&
+cd /tmp/singularity-python && sudo python3 setup.py install
+
 echo "Start Time: $(date)." > /tmp/.shub-log 2>&1
 timeout -s KILL 2h sudo python3 -c "from singularity.build.google import run_build; run_build()" >> /tmp/.shub-log 2>&1
 ret=$?
