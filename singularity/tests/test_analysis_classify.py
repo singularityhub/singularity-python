8d7143ea74d7a1ebf34c92314fa0428bb8c2eed0
vsoch <vsochat@stanford.edu>
2017-10-31 14:44:18 -0700

    testing using shub for function

--- singularity/tests/test_analysis_classify.py
+++ singularity/tests/test_analysis_classify.py
@@ -56,11 +56,10 @@ class TestAnalysisClassify(unittest.TestCase):
     def setUp(self):
         self.tmpdir = tempfile.mkdtemp()
         self.cli = Singularity()
-        self.container = self.cli.pull('docker://ubuntu:16.04', 
-                                       pull_folder=self.tmpdir)
+        self.container = self.cli.pull('shub://vsoch/singularity-images', 
+                                        pull_folder=self.tmpdir)
         print(self.container)
-
-        self.comparator = self.cli.pull('docker://ubuntu:12.04',
+        self.comparator = self.cli.pull('shub://vsoch/hello-world',
                                          pull_folder=self.tmpdir)
         print(self.comparator)        
 
